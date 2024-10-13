6eaa1fae29f0f6bf3bcb9647eeb62dad66c5c868
Mohassan863 <mohassan863@outlook.com>
2024-10-13 19:48:02 +0300

    init

---
+++ app/src/component/prayer.jsx
@@ -0,0 +1,13 @@
+"use client"
+const prayer = ({name, time}) => {
+    return (
+   <div  className="prayer">
+
+<p className="name-prayer">{name}</p>
+<p className="time-prayer">{time}</p>
+   </div>
+
+    )
+}
+
+export default prayer;
